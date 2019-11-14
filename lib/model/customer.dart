1186af92000c6b25f051471836497f8f1f859618
m.kumar <m.kumar@in.imshealth.com>
2019-11-14 18:22:20 +0530

    Creating customer add page.

---
+++ lib/model/customer.dart
@@ -0,0 +1,10 @@
+class CustomerAddForm {
+  String firstName = '';
+  String lastName = '';
+  String phoneNumber = '';
+  String seal = '';
+
+  save() {
+    print('saving user using a web service');
+  }
+}
--- lib/screens/customer_add.dart
+++ lib/screens/customer_add.dart
@@ -1,3 +1,4 @@
+import 'package:clima/model/customer.dart';
 import 'package:flutter/material.dart';
 
 class CustomerAdd extends StatefulWidget {
@@ -6,8 +7,80 @@ class CustomerAdd extends StatefulWidget {
 }
 
 class _CustomerAddState extends State<CustomerAdd> {
+  final _formKey = GlobalKey<FormState>();
+  final _customer = CustomerAddForm();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: Text('Profile')),
+        body: Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Builder(
+                builder: (context) => Form(
+                    key: _formKey,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextFormField(
+                            decoration:
+                                InputDecoration(labelText: 'First name'),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter your first name';
+                              }
+                            },
+                            onSaved: (val) =>
+                                setState(() => _customer.firstName = val),
+                          ),
+                          TextFormField(
+                              decoration:
+                                  InputDecoration(labelText: 'Last name'),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter your last name.';
+                                }
+                              },
+                              onSaved: (val) =>
+                                  setState(() => _customer.lastName = val)),
+                          TextFormField(
+                              decoration:
+                                  InputDecoration(labelText: 'Phone Number'),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter your phone number.';
+                                }
+                              },
+                              onSaved: (val) =>
+                                  setState(() => _customer.phoneNumber = val)),
+                          TextFormField(
+                              decoration: InputDecoration(labelText: 'Seal'),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter your seal';
+                                }
+                              },
+                              onSaved: (val) =>
+                                  setState(() => _customer.seal = val)),
+                          Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 16.0),
+                              child: RaisedButton(
+                                  onPressed: () {
+                                    final form = _formKey.currentState;
+                                    if (form.validate()) {
+                                      form.save();
+                                      _customer.save();
+                                      _showDialog(context);
+                                    }
+                                  },
+                                  child: Text('Save'))),
+                        ])))));
+  }
+
+  _showDialog(BuildContext context) {
+    Scaffold.of(context)
+        .showSnackBar(SnackBar(content: Text('Submitting form')));
   }
 }
